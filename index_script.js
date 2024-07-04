3039c90f9261b168bcf5ba0304bdfe5ebaac03dc
Ja Tar <tarnogorskijan@gmail.com>
2024-07-04 10:59:11 +0200

    main page, iframe, template js

---
+++ index_script.js
@@ -0,0 +1,11 @@
+document.getElementById("button_new_tab").addEventListener("click", function() {
+    openInNewTab();
+});
+
+document.getElementById("button_submit").addEventListener("click", function() {
+    generateFromTemplate();
+});
+
+function generateFromTemplate() {
+    
+}
